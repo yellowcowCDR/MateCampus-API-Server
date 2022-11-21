9419ccadc88abfecfd6f819004d257480dc9ccbf
JinYeongKim <handayoung11@gmail.com>
2022-11-21 13:36:34 +0900

    RefreshTokenRepository.findByRefreshToken method 제거

--- src/main/java/com/litCitrus/zamongcampusServer/repository/jwt/RefreshTokenRepository.java
+++ src/main/java/com/litCitrus/zamongcampusServer/repository/jwt/RefreshTokenRepository.java
@@ -9,6 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    Optional<RefreshToken> findByRefreshToken(String refreshToken);
     Optional<RefreshToken> findByRefreshTokenAndAccessToken(String refreshToken, String accessToken);
 }
