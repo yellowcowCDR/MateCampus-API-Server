de1135605e5e2f9455a1a4da9bb09247c9c540cf
JinYeongKim <handayoung11@gmail.com>
2022-12-05 16:37:59 +0900

    회원가입 시 open api와 연동되어 학과처리되도록 UserService.signup() 함수 수정

--- src/main/java/com/litCitrus/zamongcampusServer/repository/major/MajorRepository.java
+++ src/main/java/com/litCitrus/zamongcampusServer/repository/major/MajorRepository.java
@@ -4,6 +4,9 @@ import com.litCitrus.zamongcampusServer.domain.major.Major;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MajorRepository extends JpaRepository<Major, Long> {
+    Optional<Major> findByMajorSeqAndName(long majorSeq, String name);
 }
