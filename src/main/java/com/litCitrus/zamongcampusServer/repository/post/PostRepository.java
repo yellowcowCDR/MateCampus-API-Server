66839ff03564b044f345ef0e8941051a58d246ce
JinYeongKim <handayoung11@gmail.com>
2023-01-17 20:20:32 +0900

    findAllByDeletedFalseOrderByCreatedAtDesc() 함수가 searchPosts()로 대체되어 제거

--- src/main/java/com/litCitrus/zamongcampusServer/repository/post/PostRepository.java
+++ src/main/java/com/litCitrus/zamongcampusServer/repository/post/PostRepository.java
@@ -3,7 +3,6 @@ package com.litCitrus.zamongcampusServer.repository.post;
 import com.litCitrus.zamongcampusServer.domain.post.Post;
 import com.litCitrus.zamongcampusServer.domain.user.User;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -20,7 +19,6 @@ public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
 
     List<Post> findAllByUserAndDeletedFalse(User user, Pageable page); // 게시물(작성자 기준)
     List<Post> findAllByUserAndDeletedFalseOrderByCreatedAtDesc(User user, Pageable page); // 게시물(작성자 기준 최신순)
-    List<Post> findAllByDeletedFalseOrderByCreatedAtDesc(Pageable page); // 최신순
     // 인기순: likeCount > viewCount > createDesc
     List<Post> findAllByDeletedFalseOrderByLikeCountDescViewCountDescCreatedAtDesc(Pageable page);
 
