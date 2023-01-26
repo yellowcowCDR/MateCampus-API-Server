98bd358cfc47768f01637ca223288edbbdff92ab
JinYeongKim <handayoung11@gmail.com>
2023-01-26 11:21:52 +0900

    PostSearch Setter 제거

--- src/main/java/com/litCitrus/zamongcampusServer/dto/post/PostSearch.java
+++ src/main/java/com/litCitrus/zamongcampusServer/dto/post/PostSearch.java
@@ -4,16 +4,14 @@ import com.litCitrus.zamongcampusServer.domain.user.CollegeCode;
 import com.litCitrus.zamongcampusServer.domain.user.User;
 import com.litCitrus.zamongcampusServer.util.SecurityUtil;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class PostSearch {
-    private User loggedUser = SecurityUtil.getUser();
-    private User writer;
-    private CollegeCode collegeCode;
+    private final User loggedUser = SecurityUtil.getUser();
+    private final User writer;
+    private final CollegeCode collegeCode;
     //첫 페이지일 경우 null
-    private Long oldestPost;
+    private final Long oldestPost;
     private int pageSize = 10;
 
     public PostSearch(User writer, CollegeCode collegeCode, Long oldestPost) {
