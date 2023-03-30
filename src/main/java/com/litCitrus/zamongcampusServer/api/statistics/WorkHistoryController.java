9aafe97854589b2e2884fffe1265f948f6e15c1a
JinYeongKim <handayoung11@gmail.com>
2023-03-30 23:40:21 +0900

    WorkHIstoryController와 WorkHistoryService 의존성 주입 수정

--- src/main/java/com/litCitrus/zamongcampusServer/api/statistics/WorkHistoryController.java
+++ src/main/java/com/litCitrus/zamongcampusServer/api/statistics/WorkHistoryController.java
@@ -5,13 +5,15 @@ import com.litCitrus.zamongcampusServer.domain.history.WorkHistoryType;
 import com.litCitrus.zamongcampusServer.service.history.WorkHistoryService;
 import com.litCitrus.zamongcampusServer.util.IpUtil;
 import com.litCitrus.zamongcampusServer.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class WorkHistoryController {
 
-    WorkHistoryService workHistoryService;
+    private final WorkHistoryService workHistoryService;
 
     @PostMapping("/work-history")
     public void saveWorkHistory(WorkHistoryType.WorkType workType, WorkHistoryType.FunctionType functionType) {
--- src/main/java/com/litCitrus/zamongcampusServer/service/history/WorkHistoryService.java
+++ src/main/java/com/litCitrus/zamongcampusServer/service/history/WorkHistoryService.java
@@ -5,12 +5,14 @@ import com.litCitrus.zamongcampusServer.domain.history.WorkHistoryType;
 import com.litCitrus.zamongcampusServer.repository.history.WorkHistoryRepository;
 import com.litCitrus.zamongcampusServer.util.IpUtil;
 import com.litCitrus.zamongcampusServer.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class WorkHistoryService {
 
-    WorkHistoryRepository workHistoryRepository;
+    private final WorkHistoryRepository workHistoryRepository;
 
     public void saveWorkHistory(WorkHistoryType.WorkType workType, WorkHistoryType.FunctionType functionType) {
         workHistoryRepository.save(WorkHistory.createWorkHistory(workType, functionType, SecurityUtil.getUser(), IpUtil.getClientIP()));
